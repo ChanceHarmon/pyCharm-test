67215dca684a5d1bbbc127d0fb4d6d0a7275152b
Chance Harmon <henrikillsit@gmail.com>
2019-11-11 22:34:32 -0800

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+print('hello world')
+user_name = 'Chance'
+user_age = '34'
+print('Hello, my name is ' + user_name + ' and I am ' + user_age + ' years old!')
+user_name = 'Jaquelyn'
+user_age = '36'
+print('Hello, my name is ' + user_name + ' and I am ' + user_age + ' years old!')
