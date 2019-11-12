7757c47d350cf5222d5f59d4b8a0e6ee3ef5429c
Chance Harmon <henrikillsit@gmail.com>
2019-11-11 23:00:32 -0800

    strings practice

--- test.py
+++ test.py
@@ -1,7 +1,38 @@
-print('hello world')
-user_name = 'Chance'
-user_age = '34'
-print('Hello, my name is ' + user_name + ' and I am ' + user_age + ' years old!')
-user_name = 'Jaquelyn'
-user_age = '36'
-print('Hello, my name is ' + user_name + ' and I am ' + user_age + ' years old!')
+# Proof of Life
+
+# print('hello world')
+# user_name = 'Chance'
+# user_age = '34'
+# print('Hello, my name is ' + user_name + ' and I am ' + user_age + ' years old!')
+# user_name = 'Jaquelyn'
+# user_age = '36'
+# print('Hello, my name is ' + user_name + ' and I am ' + user_age + ' years old!')
+
+# strings
+
+# print("Harmon\nAcademy")
+# print("Harmon\"Academy")
+# print("Harmon\Academy")
+#
+# phrase = "Harmon Academy"
+# print(phrase)
+# print(phrase + " is legit.")
+# print(phrase.lower() + " is legit.")
+# print(phrase.upper() + " is legit.")
+# print(phrase.isupper())
+# print(phrase.upper().isupper())
+# print(phrase.islower())
+# print(phrase.lower().islower()
+# print(len(phrase))
+# print(phrase[0])
+# print(phrase[4])
+# # 0 index like JS
+# print(phrase.index("a"))
+# print(phrase.replace("Harmon", "Gladiator"))
+
+# Numbers
+
+
+
+
+
