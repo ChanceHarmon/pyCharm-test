6e463565e35fd29b668207b255b09663123944b6
Chance Harmon <henrikillsit@gmail.com>
2021-04-20 09:57:13 -0700

    dictionary and while loops

--- test.py
+++ test.py
@@ -198,6 +198,34 @@ print(month_conversions.get('Sas', 'Default text I added'))
 
 # While loops
 
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+# mini guess game
+
+# super_secret = 'banana'
+# guess = ''
+# guess_count = 0
+# guess_limit = 3
+# out_of_guesses = False
+#
+# while guess != super_secret and not out_of_guesses:
+#     if guess_count < guess_limit:
+#         guess = input('Enter your guess: ')
+#         guess_count += 1
+#     else:
+#         out_of_guesses = True
+#
+# if out_of_guesses:
+#     print('You lost!')
+# else:
+#     print('Great guess')
+
+# for loops
+
+
 
 
 
